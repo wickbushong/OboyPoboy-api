5b45e5d0820f6b4a0d1461e3ba1063a2fa90190c
wickbushong <wickbushong@gmail.com>
2021-01-13 17:31:15 -0600

    set up user login route.

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,6 @@ const User = require('../models/User')
 const router = express.Router()
 
 router.post('/users', async (req, res) => {
-    // Create a new user
     try {
         const user = new User(req.body)
         await user.save()
@@ -13,4 +12,19 @@ router.post('/users', async (req, res) => {
     } catch (error) {
         res.status(400).send(error)
     }
+})
+
+router.post('/users/login', async(req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findByCredentials(email, password)
+        if (!user) {
+            return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+        }
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
 })
