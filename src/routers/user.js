be62514a9cb87acecff8209de587c5c0748ba78b
wickbushong <wickbushong@gmail.com>
2021-01-13 19:23:59 -0600

    Set up logout route

--- src/routers/user.js
+++ src/routers/user.js
@@ -35,4 +35,16 @@ router.get('/users/me', auth, async(req, res) => {
     res.send(req.user)
 })
 
+router.post('/users/me/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 module.exports = router
