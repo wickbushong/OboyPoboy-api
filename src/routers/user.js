47e6d9edc523747f275e18688550c8a2347060ea
wickbushong <wickbushong@gmail.com>
2021-01-13 19:13:47 -0600

    Set up and test user get route.

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/User')
+const auth = require('../middleware/auth')
 
 const router = express.Router()
 
@@ -28,6 +29,10 @@ router.post('/users/login', async(req, res) => {
         res.status(400).send(error)
     }
 
+})
+
+router.get('/users/me', auth, async(req, res) => {
+    res.send(req.user)
 })
 
 module.exports = router
